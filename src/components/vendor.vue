43b2b085c3d6838381479d75be63cd17d0d173d8
agung3wi <agungskripsi05@gmail.com>
2021-03-17 16:59:22 +0700

    cc

--- src/components/vendor.vue
+++ src/components/vendor.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <input v-model="nama" type="text" name="" id="a" placeholder="nama">
-    <input v-model="alamat" type="text" name="" id="b" placeholder="alamat">
-    <input v-model="email" type="text" name="" id="c" placeholder="email">
-    <button type="button" @click="submitVendor(nama)">Tambah Vendor</button>
+    <input v-model="nama" type="text" name="" id="a" placeholder="nama" />
+    <input v-model="alamat" type="text" name="" id="b" placeholder="alamat" />
+    <input v-model="email" type="text" name="" id="c" placeholder="email" />
+    <button type="button" @click="submitVendor()">Tambah Vendor</button>
     <br />
     <pre>{{ vendor }}</pre>
     <table class="table table-striped table-inverse table-responsive">
@@ -15,9 +15,9 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(v,index) in vendor" :key="index">
-          <td>{{ v.nama }}</td>
-          <td>{{ v.alamat }}</td>
+        <tr v-for="(v, index) in vendor" :key="index">
+          <td>{{ v.vendor_name }}</td>
+          <td>{{ v.vendor_address }}</td>
           <td>{{ v.email }}</td>
         </tr>
       </tbody>
@@ -26,43 +26,59 @@
 </template>
 <script>
 export default {
-  name: 'table',
-  data () {
+  name: "table",
+  data() {
     return {
-      alamat: '',
-      nama: 'Adinda Yahya Hexatama',
-      email: '',
-      vendor: []
-    }
+      alamat: "",
+      nama: "Adinda Yahya Hexatama",
+      email: "",
+      vendor: [],
+    };
   },
-  beforeCreate () { // 1
-    console.log('before-created', this.nama)
+  created() {
+    // 2
+    this.getVendor();
   },
-  created () { // 2
-    this.submitVendor('lalala')
-    console.log('created', this.nama)
-    this.vendor.push({ 'nama': 'nama-created', 'alamat': 'alamat-created', 'email': 'email-created' })
+  beforeMount() {
+    // 3
+    console.log("before-mount", this.nama);
   },
-  beforeMount () { // 3
-    console.log('before-mount', this.nama)
+  mounted() {
+    // 4
+    this.vendor.push({
+      nama: "nama-mounted",
+      alamat: "alamat-mounted",
+      email: "email-mounted",
+    });
   },
-  mounted () { // 4
-    console.log('mounted', this.nama)
-    this.vendor.push({ 'nama': 'nama-mounted', 'alamat': 'alamat-mounted', 'email': 'email-mounted' })
-  },
-  beforeDestroy () { // 5
-    alert('Before-Destroyed')
-  },
-  destroyed () { // 6
-    alert('Destroyed')
-  },
+
   methods: {
-    submitVendor (nama) {
-      this.vendor.push({ 'nama': nama, 'alamat': this.alamat, 'email': this.email })
-      this.nama = ''
-      this.alamat = ''
-      this.email = ''
-    }
-  }
-}
+    submitVendor() {
+      let input = {
+        vendor_name: this.nama,
+        vendor_address: this.alamat,
+        email: this.email,
+      };
+      this.$http
+        .post("http://localhost:8000/vendor/create", input)
+        .then((response) => {
+          alert("Sukses");
+          this.getVendor();
+        })
+        .catch((err) => {
+          alert("Error");
+        });
+    },
+    getVendor() {
+      this.$http
+        .get("http://localhost:8000/vendor")
+        .then((response) => {
+          this.vendor = response.data;
+        })
+        .catch((err) => {
+          alert("Error");
+        });
+    },
+  },
+};
 </script>
